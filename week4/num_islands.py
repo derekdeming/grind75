40636052016bd268c2c4c1b67eeb504ab17bd094
Derek <derekdeming17@gmail.com>
2023-11-25 12:53:06 -0500

    partial explanation -- dfs is easy

--- week4/num_islands.py
+++ week4/num_islands.py
@@ -3,20 +3,20 @@
 from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
+        if not grid: #grid = empty 
             return 0
 
         num_islands = 0
 
-        # Function to perform DFS on the grid
+        # depth first search on grid -- 
         def dfs(i, j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
                 return
 
-            # Mark the land as visited by setting it to '0'
+            # setting it to 0 if seen
             grid[i][j] = '0'
 
-            # Visit all adjacent lands
+            # should traverse the adjacent parts 
             dfs(i + 1, j)
             dfs(i - 1, j)
             dfs(i, j + 1)
@@ -24,9 +24,13 @@ class Solution:
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                # If land is found, perform DFS and increment island count
+                # if found use DFS and increment island count
                 if grid[i][j] == '1':
                     dfs(i, j)
                     num_islands += 1
 
         return num_islands
+
+'''
+use dfs on unvisited cell. it should traverse and mark connected cells 
+'''
